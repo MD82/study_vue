f5960f740bba4fbbf2d3f85ea897ef43fab0aec8
md_home <jlkmh82@msn.com>
2022-10-20 23:34:44 +0900

    [Test] Lesson7 - init TicTacToe

---
+++ TdComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+export default {
+  props: {
+    cellData: String,
+    rowIndex: Number,
+    cellIndex: Number,
+  },
+  methods: {
+    onClickTd() {
+      console.log(this.$root.$data);
+      console.log(this.$parent.$data);
+      this.$root.$data.true = this.$root.$data.true === 'O' ? 'X' : 'O';
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
